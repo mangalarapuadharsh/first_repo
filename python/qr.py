0f64144a54276df69e5e321d2e803e71db74937c
Mangalarapu Adharsh <mangalarapuadharsh@gmail.com>
2026-01-02 00:32:16 +0530

    uplaod

---
+++ python/qr.py
@@ -0,0 +1,22 @@
+import qrcode
+url =input("enter url: ")
+file="C:\\Users\\rider\\Downloads\\qrcode.png"
+img = qrcode.make(url)
+img.save(file)
+print("qr generator successfully created at "+file)
+a=int(input("enter 1 to open the file:"))
+# print("helolo wolrd")
+
+if a==1:
+    import os
+    os.startfile(file)
+    # print("press any key to exit")
+    os.system("pause")
+    os._exit(0)
+
+
+else:
+    print("ok bye")
+
+
+    
